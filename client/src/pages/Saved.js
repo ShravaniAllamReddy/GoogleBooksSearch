d885ee746ebd9572302e6380a7e8da9d7f448fe4
shravaniallamreddy <shravani.allam@gmail.com>
2021-01-20 22:35:32 -0700

    added comments

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,53 +1,55 @@
 import React, { Component } from "react";
 import API from "../utils/API";
 import { Container, Row, Col } from "../components/Grid";
-import SavedResult from "../components/SavedResult"
+import SavedResult from "../components/SavedResult";
 import Jumbotron from "../components/Jumbotron";
 
 class SaveBooks extends Component {
-    //create state
-    state = {
-        savedbooks: [],
-    };
-    
-    componentDidMount(){
-        this.fetchSavedBooks();
-    }
+  //sets the initial state
+  state = {
+    savedbooks: [],
+  };
 
-    fetchSavedBooks() {
-        API.savedbooks().then(res => {
-            this.setState({ savedbooks: res.data })
-        })
-    }
+  //default method which executes when page loads
+  componentDidMount() {
+    this.fetchSavedBooks();
+  }
 
-    handleDeleteButton = id => {             
-              API.deleteBook(id)
-                  .then( res => this.fetchSavedBooks())
-                  .catch(err => console.log(err))
-          }
+  //fetches the savedbooks from db
+  fetchSavedBooks() {
+    API.savedbooks().then((res) => {
+      this.setState({ savedbooks: res.data });
+    });
+  }
 
-    render() {
-        return (
-           <div>
-              <Jumbotron>
-                <h2>(React) Google Books Search</h2>
-                <p>Search for and Save Books of Interest</p>
-              </Jumbotron>
-                <br></br>
-                <Container>
-                    <Row>
-                        <Col size="12">
-                          <SavedResult
-                            savedbooks={this.state.savedbooks} 
-                            handleDeleteButton= {this.handleDeleteButton}
-                          />
-                        </Col>
-                    </Row>
-                </Container>
-           </div>
-        )
-    }
+  //delets the saved book from database
+  handleDeleteButton = (id) => {
+    API.deleteBook(id)
+      .then((res) => this.fetchSavedBooks())
+      .catch((err) => console.log(err));
+  };
 
+  render() {
+    return (
+      <div>
+        <Jumbotron>
+          <h2>(React) Google Books Search</h2>
+          <p>Search for and Save Books of Interest</p>
+        </Jumbotron>
+        <br></br>
+        <Container>
+          <Row>
+            <Col size="12">
+              <SavedResult
+                savedbooks={this.state.savedbooks}
+                handleDeleteButton={this.handleDeleteButton}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default SaveBooks;
